75c09c4e1dd64ddf82c5820dc95913b49711dd89
Manish <rdjmanishoo7@gmail.com>
2026-03-05 13:26:04 +0530

    add start

---
+++ src/main/java/com/user/main/Main.java
@@ -0,0 +1,14 @@
+package com.user.main;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Starting Application");
+        SpringApplication.run(Main.class, args);
+    }
+}
